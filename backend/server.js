0e59c70934b5fcfce1118f54cd338c458ff51115
Jennifer Stephens <xxjennandtonicxx@gmail.com>
2023-04-29 20:42:44 -0700

    fixed config path

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,33 @@
+// // DEPENDENCIES
+// const express = require('express');
+// const cors = require('cors');
+// const app = express();
+// const { sequelize } = require('sequelize');
+// const path = require('path');
+
+// // MIDDLEWARE
+// require('dotenv').config();
+// app.use(cors());
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+
+// // CONTROLLERS
+// const usersController = require('./controllers/users_controller.js');
+// app.use('/api/users', usersController);
+
+// const workoutsController = require('./controllers/workouts_controller.js');
+// app.use('/api/workouts', workoutsController);
+
+// // LISTEN
+// app.listen(4000, () => {
+//     console.log(`Server is running on port 4000`);
+// });
+
 // DEPENDENCIES
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const { sequelize } = require('sequelize');
+const { Sequelize } = require('sequelize');
 const path = require('path');
 
 // MIDDLEWARE
@@ -11,14 +36,9 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// CONTROLLERS
-const usersController = require('./controllers/users_controller.js');
-app.use('/api/users', usersController);
-
-const workoutsController = require('./controllers/workouts_controller.js');
-app.use('/api/workouts', workoutsController);
-
 // LISTEN
 app.listen(4000, () => {
     console.log(`Server is running on port 4000`);
 });
+
+console.log("hello world")
