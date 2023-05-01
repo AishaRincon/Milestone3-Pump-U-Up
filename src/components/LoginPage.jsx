f7419fa755503e4ba733ca422f07b819849dd03d
benjamin farmer <viroben2001@gmail.com>
2023-05-01 12:20:11 -0700

    added css in loging componet with logo

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -48,17 +48,17 @@ const handleLoginSubmit = async (e) => {
                     src={require("../assets/logo.png")}
                     alt="Pump-U-Up logo, a barbell with weights and a muscular arm busting through the middle saying 'Pump-U-Up'." />
       </div>
-      <div className="container">
+      <div className="container ">
         <div className="container-two"></div> {/* should this container have a different name? */}
           <Button variant="primary" onClick={handleLoginClick}>
             {showLoginForm ? 'Cancel' : 'Login'}
           </Button>
         </div>
         {showLoginForm && (
-          <Form onSubmit={handleLoginSubmit}>
+          <Form onSubmit={handleLoginSubmit} className=''>
             <Form.Group controlId="formBasicUsername"> {/* form-group is a bootstrap class */} 
-              <Form.Label>Username</Form.Label> 
-              <Form.Control type="text" placeholder="Enter username" value={username} onChange={handleUsernameChange} />
+              <Form.Label>Username </Form.Label > 
+              <Form.Control type="text"  placeholder="Enter username" value={username} onChange={handleUsernameChange} />
               <Form.Label>Password</Form.Label> 
               <Form.Control type="password" placeholder="Enter password" value={password} onChange={handlePasswordChange} />
             </Form.Group>
