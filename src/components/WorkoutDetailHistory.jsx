c504bf032f8593c769a2932d19687f6264e33388
benjamin farmer <viroben2001@gmail.com>
2023-04-30 19:51:51 -0700

    changed code in the workoutdetails history

--- src/components/WorkoutDetailHistory.jsx
+++ src/components/WorkoutDetailHistory.jsx
@@ -2,38 +2,42 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Navbar from './Navbar';
 
-const WorkoutDetailHistory = ({ workoutHistory }) => {
+import { useState, useEffect } from 'react';
+
+function WorkoutDetailHistory(props) {
+  const [workout, setWorkout] = useState(null);
+
+  useEffect(() => {
+    async function fetchWorkout() {
+      try {
+        const response = await fetch(`${process.env.REACT_APP_FETCH_URI}/api/workouts/${props.id}`);
+        const data = await response.json();
+        setWorkout(data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    fetchWorkout();
+  }, [props.id]);
+
+  if (!workout) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div>
-      <Navbar />
-      <h2>Workout History</h2>
-      {workoutHistory.length === 0 ? (
-        <p>No workout history available.</p>
-      ) : (
-        <table>
-          <thead>
-            <tr>
-              <th>Date</th>
-              <th>Workout Name</th>
-              <th>Duration</th>
-              <th>Calories Burned</th>
-            </tr>
-          </thead>
-          <tbody>
-            {workoutHistory.map((workout) => (
-              <tr key={workout.id}>
-                <td>{workout.date}</td>
-                <td>{workout.name}</td>
-                <td>{workout.duration}</td>
-                <td>{workout.caloriesBurned}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      )}
-      <Link to="/add-workout">Add Workout</Link>
+      <h2>{workout.name}</h2>
+      <p>{workout.description}</p>
+      <ul>
+        {workout.exercises.map(exercise => (
+          <li key={exercise.id}>
+            <strong>{exercise.name}</strong>: {exercise.sets} x {exercise.reps}
+          </li>
+        ))}
+      </ul>
     </div>
   );
-};
+}
 
 export default WorkoutDetailHistory;
