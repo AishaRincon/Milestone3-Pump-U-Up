7595c052b137878247c02da1d46afab8356bf3ff
AishaRincon <100747969+AishaRincon@users.noreply.github.com>
2023-04-30 18:12:18 -0700

    created navbar with routed buttons, and added route to workoutlist

--- frontend/components/workoutList.jsx
+++ frontend/components/workoutList.jsx
@@ -1,37 +1,18 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
+{/* import supabase? */}
 
-function ActivityDetailsPage() {
-  const { id } = useParams();
-  const [activity, setActivity] = useState(null);
-  const [error, setError] = useState(null);
+function workoutList() {
+  const [workouts, setWorkouts] = useState([]);
 
   useEffect(() => {
-    fetch(`/api/activities/${id}`)
-      .then(res => res.json())
-      .then(
-        data => {
-          setActivity(data);
-        },
-        error => {
-          setError(error);
-        }
-      );
-  }, [id]);
-
-  if (error) {
-    return <div>Error: {error.message}</div>;
-  } else if (!activity) {
-    return <div>Loading...</div>;
-  } else {
-    return (
-      <div>
-        <h1>Activity Details Page</h1>
-        <h2>{activity.name}</h2>
-        <p>{activity.description}</p>
-        <p>Duration: {activity.duration} minutes</p>
-      </div>
-    );
+    async function fetchWorkouts() {
+      const response = await fetch('http://localhost:4000/api/workouts');
+      const data = await response.json();
+      setWorkouts(data);
+    }
+    fetchWorkouts();
   }
 }
 
