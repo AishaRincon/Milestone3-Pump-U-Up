581baadfa15a6798fe9ebed40eb71759643acaba
AishaRincon <100747969+AishaRincon@users.noreply.github.com>
2023-05-05 22:29:41 -0700

    removed un-needed files

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,3 @@
-// // DEPENDENCIES
-// const express = require('express');
-// const cors = require('cors');
-// const app = express();
-// const { sequelize } = require('sequelize');
-// const path = require('path');
-
-// // MIDDLEWARE
-// require('dotenv').config();
-// app.use(cors());
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-
-
-
-// const workoutsController = require('./controllers/workouts_controller.js');
-// app.use('/api/workouts', workoutsController);
-
-// // LISTEN
-// app.listen(4000, () => {
-//     console.log(`Server is running on port 4000`);
-// });
-
 // DEPENDENCIES
 const express = require('express');
 const app = express();
@@ -51,10 +28,6 @@ app.use('/api/users', usersController);
 
 // const workoutsController = require('./controllers/workouts_controller.js');
 // app.use('/api/workouts', workoutsController);
-
-
-
-
 
 // LISTEN
 app.listen(8080, () => {
